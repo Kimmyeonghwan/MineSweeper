a5af6e06e54801213d114c6c933bfc2bc96ef8e5
Myeonghwan Kim <myeonghwan2@kookmin.ac.kr>
2018-12-01 04:16:00 +0900

    testGetStatus에서 unknowns 값이 GAME OVER, GAME CLEAR, 배열 값 일 경우 모두 찾아서 테스트하는 코드 작성!

--- testModel.py
+++ testModel.py
@@ -8,6 +8,13 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.g1 = Model()
 
+        self.g1.unknowns = 5
+        self.g1.mineNumbers = 0
+        self.g1.answer = []
+
+        self.g1.size = 8
+        self.g1.mine = [[0 for x in range(self.g1.size + 2)] for y in range(self.g1.size + 2)]
+        self.g1.current = [['_' for x in range(self.g1.size + 2)] for y in range(self.g1.size + 2)]
     def tearDown(self):
         pass
 
@@ -18,25 +25,21 @@ class TestModel(unittest.TestCase):
         self.assertEqual(self.g1.size, 8)
 
     def testGetStatus(self):
-        self.g1.unknowns = 5
-        self.g1.mineNumbers = 0
-        self.g1.answer = []
-
         self.assertFalse(self.g1.getStatus(False))
         self.assertTrue(self.g1.getStatus(True))
 
         self.g1.getStatus(True)
         self.assertEqual(self.g1.unknowns, 'GAME OVER')
 
-        self.g1.unknowns = self.g1.mineNumbers
+
+        self.g1.getStatus(False)
+        self.assertEqual(self.g1.unknowns, 64)
+
+        self.g1.mineNumbers = self.g1.unknowns
         self.g1.getStatus(False)
         self.assertEqual(self.g1.unknowns, 'GAME CLEAR')
 
     def testGuess(self):
-        self.g1.size = 8
-        self.g1.mine = [[0 for x in range(self.g1.size + 2)] for y in range(self.g1.size + 2)]
-        self.g1.current = [['_' for x in range(self.g1.size + 2)] for y in range(self.g1.size + 2)]
-
         self.assertTrue(self.g1.guess(9, 1))
         self.assertTrue(self.g1.guess(0, 1))
 
